cf85cdad424b8ff69bb42ddbedfb87f97c772c69
Anshul Sinha <anshulsweb@gmail.com>
2023-04-15 20:29:57 -0700

    Add files via upload

---
+++ person.dart
@@ -0,0 +1,11 @@
+import 'club.dart';
+
+class Person {
+  String firstName;
+  String lastName;
+  String dob;
+  List<Person> friends;
+  List<Club> clubs;
+
+  Person(this.firstName, this.lastName, this.dob, this.friends, this.clubs);
+}
